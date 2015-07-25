71a709c446efb6f9cc900cd9a405c5d07a89a467
Mr. Gash <Mr-Gash@users.noreply.github.com>
2015-07-24 19:48:46 -0700

    Update staffchat.lua

--- staffchat.lua
+++ staffchat.lua
@@ -6,65 +6,73 @@ end
 
 hook.Add( "PlayerSay", "Staff Chat", function( ply, text )
 	if not isAdmin( ply ) then return end
+	if text:Left( 1 ) ~= "@" then return end
 
-	if text:Left( 1 ) == "@" then
-		local start = 2
+	if text:lower():Left( 3 ) == "@p " then
+		local rest = text:sub( 4, #text )
+		local pomf = string.Explode( " ", rest )[ 1 ]
 
-		if text:lower():Left( 3 ) == "@p " then
-			local rest = text:sub( start + 2, #text )
-			local pomf = string.Explode( " ", rest )[ 1 ]
+		local targ
+		local soft = false
+		for k, v in pairs( player.GetAll() ) do
+			if not string.find( v:Nick():lower(), pomf:lower() ) then continue end
 
-			local targ
-			local soft = false
-			for k, v in pairs( player.GetAll() ) do
-				if not string.find( v:Nick():lower(), pomf:lower() ) then continue end
-
-				if v:Nick() == pomf then
-					targ = v
-					break
-				elseif v:Nick():lower() == pomf:lower() then
-					targ = v
-					soft = true
-				elseif not soft then
-					targ = v
-				end
+			if v:Nick() == pomf then
+				targ = v
+				break
+			elseif v:Nick():lower() == pomf:lower() then
+				targ = v
+				soft = true
+			elseif not soft then
+				targ = v
 			end
+		end
 
-			if not targ then
-				ply:ChatPrint( "No target found!" )
-				return ""
-			end
+		if not targ then
+			ply:ChatPrint( "No target found!" )
+			return ""
+		end
 
-			local send = rest:sub( #pomf + 2, #text )
-			targ:ChatPrint( "(From " .. ply:Nick() .. ") " .. send )
+		local send = rest:sub( #pomf + 2, #text )
+		targ:ChatPrint( "(From " .. ply:Nick() .. ") " .. send )
 
-			local str = "(" .. ply:Nick() .. " -> " .. targ:Nick() .. ") " .. send
-			ServerLog( "SC: " .. str .. "\n" )
+		local str = "(" .. ply:Nick() .. " -> " .. targ:Nick() .. ") " .. send
+		ServerLog( "SC: " .. str .. "\n" )
 
-			if not isAdmin( targ ) then
-				for k, v in pairs( player.GetAll() ) do
-					if not isAdmin( v ) then continue end
-
-					v:ChatPrint( str )
-				end
-			else ply:ChatPrint( "(To " .. targ:Nick() .. ") " .. send ) end
-		else
-			local is_all = text:Left( start ) == "@!"
-			if is_all then start = start + 1 end
-			start = text:sub( start, start ) == " " and start + 1 or start
-
-			local send = text:sub( start, #text )
-			local str = ( is_all and "(GLOBAL) " or "(STAFF) " ) .. ply:Nick() .. ": " .. send
-
+		if not isAdmin( targ ) then
 			for k, v in pairs( player.GetAll() ) do
-				if not is_all and not isAdmin( v ) then continue end
+				if not isAdmin( v ) then continue end
 
 				v:ChatPrint( str )
 			end
+		else ply:ChatPrint( "(To " .. targ:Nick() .. ") " .. send ) end
+	elseif text:lower():Trim() == "@" then
+		for k, v in pairs( {
+			"staff only chat: @ text",
+			"private messaging: @ playername text",
+			"- staff to nonstaff are shown to all staff. staff to staff are private.",
+			"global messaging: @! text",
+		} ) do
+			ply:ChatPrint( v )
+		end
+	else
+		local start = 2
+		local is_all = text:Left( start ) == "@!"
 
-			ServerLog( "SC: " .. str .. "\n" )
+		if is_all then start = start + 1 end
+		if text:sub( start, start ) == " " then start = start + 1 end
+
+		local send = text:sub( start, #text )
+		local str = ( is_all and "(GLOBAL) " or "(STAFF) " ) .. ply:Nick() .. ": " .. send
+
+		for k, v in pairs( player.GetAll() ) do
+			if not is_all and not isAdmin( v ) then continue end
+
+			v:ChatPrint( str )
 		end
 
-		return ""
+		ServerLog( "SC: " .. str .. "\n" )
 	end
+
+	return ""
 end )

c36088937569b4204c4ecb60bbbc8409150768b4
Sunzi <Mr-Gash@users.noreply.github.com>
2015-08-21 13:50:53 -0700

    Create fakeplay.lua
    untested

---
+++ fakeplay.lua
@@ -0,0 +1,44 @@
+--[[
+	Function: surface.fPlaySound( table info[, ply ] )
+	
+	This fakes playing a song through atmosphere and plays a local song file.
+
+	If this is called on the server, the second argument can be the player to
+	play the song on. If called on the client, it only takes the first argument.
+
+	Example: surface.fPlaySound( {
+		file = "mycoolsound.mp3",
+		artist = "Hello",
+		song = "world!",
+		length = 13,
+		endround = true,
+	} )
+]]
+
+if SERVER then
+	AddCSLuaFile()
+
+	util.AddNetworkString( "atmosphere FakePlay" )
+	function surface.fPlaySound( info, ply )
+		if not atmosphere then return error( "Atmosphere isn't installed!" ) end
+		if not ( info.file and info.artist and info.song and tonumber( info.length ) ) then return error( "Invalid/missing args" ) end
+		net.Start( "atmosphere FakePlay" )
+			net.WriteTable( info )
+		if ply then net.Send( ply ) else net.Broadcast() end
+	end
+
+	return
+end
+
+function surface.fPlaySound( info )
+	if atmosphere.current and IsValid( atmosphere.panel ) then return end
+	if info.endround and GetConVarNumber( "ttt_atmosphere_endround" ) ~= 1 then return end
+
+	surface.PlaySound( info.file )
+	atmosphere:Play( "9BifiioaIos", info.artist, info.song, info.length, 0, CurTime(), true )
+end
+
+net.Receive( "atmosphere FakePlay", function()
+	local info = net.ReadTable()
+	surface.fPlaySound( info )
+end )
